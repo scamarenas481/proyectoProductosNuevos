d113878a57f8ab5be90ab55e1c8d18d57f7bec0b
Sergio Camarena <scamarenas481@gmail.com>
2023-08-29 16:27:59 -0600

    amos con todo alparecer ya esta todo arriba

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Estilos CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <!-- Scripts JavaScript -->
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </head>
@@ -40,7 +40,7 @@
                 </li>
             </ul>
         </li>
-        
+
         @if (auth()->user()->activeRole && (auth()->user()->activeRole->name === 'Ventas'))
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('vendedor.home') }}">Inicio Vendedor</a>
